dfe7d07dcff7a61735831e94f8f7645e00c655b3
tocgic <jeongyon.cho@gmail.com>
2020-12-28 09:14:44 +0900

    add week6 - live

---
+++ app/src/test/java/com/tocgic/exam/study_java_basic/week6/LiveTest.java
@@ -0,0 +1,16 @@
+package com.tocgic.exam.study_java_basic.week6;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 1. 책 추천
+ *  - 객체지향의 사실과 오해 (조영호)
+ *  - 오브젝트 (조영호)
+ * 2. 더블디스페치 - 토비님 강의영상 (https://youtu.be/s-tXAHub6vg)
+ * 3. IntelliJ 기능 : show bytecode
+ */
+public class LiveTest {
+    @Test
+    void name() {
+    }
+}
