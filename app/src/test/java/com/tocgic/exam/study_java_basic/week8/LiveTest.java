03d77d967a8ecdd38b9a1642eadfb59342a94089
tocgic <jeongyon.cho@gmail.com>
2021-01-16 01:21:04 +0900

    add week8 - live

---
+++ app/src/test/java/com/tocgic/exam/study_java_basic/week8/LiveTest.java
@@ -0,0 +1,19 @@
+package com.tocgic.exam.study_java_basic.week8;
+
+/**
+ * - 책소개 : 비폭력대화 - 업무, 육아 등의 대화의 기술 (빡 칠때..)
+ * - Tip : reader mode
+ * - study
+ *      - interface 의 default 는 왜? 생겼을까? - https://dev-coco.tistory.com/13
+ *      - is-a , has-a 설명 - https://k3068.tistory.com/34
+ *      - 결합도 : 강한 결합 / 느슨한 결합 - https://ahnyezi.github.io/java/javastudy-8-interface/
+ *      - 응집도
+ * - next
+ *      - 자바에서 예외 처리 방법 (try, catch, throw, throws, finally)
+ *      - 자바가 제공하는 예외 계층 구조 (최상위는?)
+ *      - Exception과 Error의 차이는?
+ *      - RuntimeException과 RE가 아닌 것의 차이는?
+ *      - 커스텀한 예외 만드는 방법
+ */
+public class LiveTest {
+}
