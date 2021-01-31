c6f607422818e9daf80e7b4af6cad8c403ccdbc3
tocgic <jeongyon.cho@gmail.com>
2021-02-01 07:23:34 +0900

    week11 live

---
+++ app/src/test/java/com/tocgic/exam/study_java_basic/week11/LiveTest.java
@@ -0,0 +1,37 @@
+package com.tocgic.exam.study_java_basic.week11;
+
+/**
+ * - 책소개
+ *   - 스프링
+ *      - 토비의 스프링 3.1
+ *   - 하이버네이트 관련
+ *      - Hibernate (하이버네잍 완벽가이드)
+ *      - (자바 ORM 기초) JPA 프로그래밍
+ *
+ * - IntelliJ tip
+ *
+ * - week11
+ *   - Type Safety [타입 세이프티] 의미 : value 뿐만 아리나, 타입까지 동일해야하는 ..
+ *      ```
+ *      static final int APPLE = 1;
+ *      if (APPLE == 1) { ... 타입 세이프티 하지 않다. }
+ *
+ *      enum Fruit { APPLE }
+ *      if (Fruit.APPLE == Comp.APPLE) { ... 타입 세이프티 하다. }
+ *      ```
+ *
+ *    - ordinal() 을 사용에 주의 해야 함
+ *
+ * - 12주차 과제 설명
+ *    - 애노테이션 정의하는 방법
+ *    - @retention
+ *    - @target
+ *    - @documented
+ *    - 애노테이션 프로세서
+ *
+ */
+public class LiveTest {
+    public static final String NAME = "";
+
+
+}
